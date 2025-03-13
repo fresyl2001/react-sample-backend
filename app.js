45f528210b416cc1adc9fe030b45f693f57c067b
fresyl2001 <fresylmaearmada78@gmail.com>
2025-03-13 18:41:29 +0800

    first commit

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const { getStoredPosts, storePosts } = require('./data/posts');
 
 const app = express();
-
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-	// Attach CORS headers
-	// Required when using a detached backend (that runs on a different domain)
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
 
 app.get('/posts', async (req, res) => {
 	const storedPosts = await getStoredPosts();
-	// await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
 	res.json({ posts: storedPosts });
-});
-
-app.get('/posts/:id', async (req, res) => {
-	const storedPosts = await getStoredPosts();
-	const post = storedPosts.find((post) => post.id === req.params.id);
-	res.json({ post });
 });
 
 app.post('/posts', async (req, res) => {
 	const existingPosts = await getStoredPosts();
-	const postData = req.body;
-	const newPost = {
-		...postData,
-		id: Math.random().toString()
-	};
-	const updatedPosts = [newPost, ...existingPosts];
-	await storePosts(updatedPosts);
+	const newPost = { ...req.body, id: Math.random().toString() };
+	await storePosts([newPost, ...existingPosts]);
 	res.status(201).json({ message: 'Stored new post.', post: newPost });
+});
+
+app.put('/posts/:id', async (req, res) => {
+	const storedPosts = await getStoredPosts();
+	const updatedPosts = storedPosts.map(post => post.id === req.params.id ? { ...post, ...req.body } : post);
+	await storePosts(updatedPosts);
+	res.json({ message: 'Post updated!', post: req.body });
+});
+
+app.delete('/posts/:id', async (req, res) => {
+	const storedPosts = await getStoredPosts();
+	const updatedPosts = storedPosts.filter(post => post.id !== req.params.id);
+	await storePosts(updatedPosts);
+	res.json({ message: 'Post deleted!' });
 });
 
 app.listen(8080);
